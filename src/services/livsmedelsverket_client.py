02898c98d62ef862e10eda9fbabb65c27f71e479
Azzty <shakingleaves1@gmail.com>
2026-07-11 18:56:48 +0200

    Added Ingredient class + testing and fixed methods using swedish column names

--- src/services/livsmedelsverket_client.py
+++ src/services/livsmedelsverket_client.py
@@ -25,6 +25,35 @@ def advanced_fuzzy_match(query:str, db_value:str):
 
     return base_score
 
+class Ingredient:
+    def __init__(self, item_id:int, name:str, protein: float, fat: float, carbs: float, calories: float, groups: list | None = None, micronutrients: dict | None = None):
+        self.id = item_id
+        self.name = name
+        self.protein = protein
+        self.fat = fat
+        self.carbs = carbs
+        self.calories = calories
+
+        self.groups = groups if groups is not None else []
+        self.micronutrients = micronutrients if micronutrients is not None else {}
+
+    @staticmethod
+    def from_livsmedelsverket_dict(db_dict: dict | sqlite3.Row):
+        data = dict(db_dict).copy()
+
+        ing_id = int(data.pop("id"))
+        ingredient_name = str(data.pop("Food_Name"))
+        protein = float(data.pop("Protein_g"))
+        fat = float(data.pop("Fat_total_g"))
+        carbs = float(data.pop("Carbohydrates_available_g"))
+        calories = int(data.pop("Energy_kcal"))
+        groups = [s.strip() for s in str(data.pop("Filter_group")).split(",")]
+
+        micronutrients = {k: float(v) if v is not None else None for k, v in data.items()}
+
+        return Ingredient(ing_id, ingredient_name, protein, fat, carbs, calories, groups, micronutrients)
+
+
 class LivsmedelsverketClient:
     def __init__(self):
         self.livsmedelsverket_conn = sqlite3.connect(ROOT_DIR / 'res/livsmedelsverket.db')
@@ -32,28 +61,28 @@ class LivsmedelsverketClient:
         self.livsmedelsverket_conn.create_function('FUZZ', 2, fuzzy_match)
         self.livsmedelsverket_conn.create_function('ADV_FUZZ', 2, advanced_fuzzy_match)
 
-    def get_food_by_id(self, id):
+    def get_food_by_id(self, db_id):
         with self.livsmedelsverket_conn as conn:
             cursor = conn.cursor()
-            cursor.execute("SELECT * FROM livsmedelsverket WHERE id = ?", (id,))
+            cursor.execute("SELECT * FROM livsmedelsverket WHERE id = ?", (db_id,))
             return cursor.fetchone()
 
     def get_food_by_number(self, number):
         with self.livsmedelsverket_conn as conn:
             cursor = conn.cursor()
-            cursor.execute("SELECT * FROM livsmedelsverket WHERE Livsmedelsnummer = ?", (number,))
+            cursor.execute("SELECT * FROM livsmedelsverket WHERE Food_Number = ?", (number,))
             return cursor.fetchone()
 
     def get_food_by_name(self, name):
         with self.livsmedelsverket_conn as conn:
             cursor = conn.cursor()
-            cursor.execute("SELECT * FROM livsmedelsverket WHERE Livsmedelsnamn = ?", (name,))
+            cursor.execute("SELECT * FROM livsmedelsverket WHERE Food_Name = ?", (name,))
             return cursor.fetchone()
 
     def search_foods_by_name(self, query, limit=20):
         with self.livsmedelsverket_conn as conn:
             sql = """
-            SELECT *, ADV_FUZZ(?, Livsmedelsnamn) as score
+            SELECT *, ADV_FUZZ(?, Food_Name) as score
             FROM livsmedelsverket
             WHERE score >= 70
             ORDER BY score DESC
--- tests/test_livsmedelsverket_service.py
+++ tests/test_livsmedelsverket_service.py
@@ -5,7 +5,7 @@ import src.services.livsmedelsverket_client as livsmedelsverket
 def test_get_valid_food_number():
     client = livsmedelsverket.LivsmedelsverketClient()
     food = client.get_food_by_number(1)
-    assert food is not None and food["Livsmedelsnamn"] == "Nöt talg"
+    assert food is not None and food["Food_Name"] == "Nöt talg"
 
 def test_get_food_number_out_of_range():
     client = livsmedelsverket.LivsmedelsverketClient()
@@ -20,7 +20,7 @@ def test_get_invalid_food_number():
 def test_get_valid_food_name():
     client = livsmedelsverket.LivsmedelsverketClient()
     food = client.get_food_by_name("Nöt talg")
-    assert food is not None and food["Livsmedelsnummer"] == 1
+    assert food is not None and food["Food_Number"] == 1
 
 def test_get_invalid_food_name():
     client = livsmedelsverket.LivsmedelsverketClient()
@@ -30,7 +30,7 @@ def test_get_invalid_food_name():
 def test_get_valid_food_id():
     client = livsmedelsverket.LivsmedelsverketClient()
     food = client.get_food_by_id(0)
-    assert food is not None and food["Livsmedelsnamn"] == "Nöt talg"
+    assert food is not None and food["Food_Name"] == "Nöt talg"
 
 def test_get_invalid_food_id():
     client = livsmedelsverket.LivsmedelsverketClient()
@@ -40,8 +40,8 @@ def test_get_invalid_food_id():
 def test_reasonable_results_from_search():
     client = livsmedelsverket.LivsmedelsverketClient()
     results = client.search_foods_by_name("mjölk", 5)
-    assert any(d.get("Livsmedelsnamn") == "Mjölk fett 3% berikad" for d in results)
-    assert any(d.get("Livsmedelsnamn") == "Mjölk fett 4,2% typ lantmjölk" for d in results)
+    assert any(d.get("Food_Name") == "Mjölk fett 3% berikad" for d in results)
+    assert any(d.get("Food_Name") == "Mjölk fett 4,2% typ lantmjölk" for d in results)
 
 def test_fuzzy_match_tolerance():
     score = livsmedelsverket.fuzzy_match("Mjlk", "Mjölk")
@@ -51,3 +51,26 @@ def test_fuzzy_match_case_insensitive():
     upper_score = livsmedelsverket.fuzzy_match("MJÖLK", "mjölk")
     lower_score = livsmedelsverket.fuzzy_match("mjölk", "mjölk")
     assert upper_score == lower_score
+
+def test_ingredient_object_from_livsmedelsverket():
+    client = livsmedelsverket.LivsmedelsverketClient()
+
+    # Simple values
+    db_dict = client.get_food_by_id(0)
+    ing = livsmedelsverket.Ingredient.from_livsmedelsverket_dict(db_dict)
+    assert ing.id == 0
+    assert ing.name == "Nöt talg"
+    assert ing.protein == 0.0
+    assert ing.fat == 100.0
+    assert ing.carbs == 0.0
+    assert ing.calories == 884
+
+    # Diverse values
+    db_dict = client.get_food_by_id(33)
+    ing = livsmedelsverket.Ingredient.from_livsmedelsverket_dict(db_dict)
+    assert ing.id == 33
+    assert ing.name == "Räkmajonnäs räksallad gatukök"
+    assert ing.protein == 5.2
+    assert ing.fat == 48.0
+    assert ing.carbs == 2.9
+    assert ing.calories == 457
