bcfaa329f380e15f258008abe08bdaefa462a2d9
Azzty <shakingleaves1@gmail.com>
2026-07-11 14:51:11 +0200

    Refactored db to have dedicated id col

--- dev/create_db_from_xslx.py
+++ dev/create_db_from_xslx.py
@@ -2,13 +2,13 @@ import sqlite3
 import pandas as pd
 from config import ROOT_DIR
 
-df = pd.read_excel(ROOT_DIR / "LivsmedelsDB_202607110024.xlsx", skiprows=2, index_col=1)
+df = pd.read_excel(ROOT_DIR / "LivsmedelsDB_202607110024.xlsx", skiprows=2)
 
 df.columns = df.columns.str.replace(r"[(),]", "", regex=True)
 df.columns = df.columns.str.replace(" ", "_")
 df.columns = df.columns.str.replace("µ", "u")
 
 with sqlite3.connect(ROOT_DIR / 'res/Livsmedelsverket.db') as conn:
-    df.to_sql('livsmedelsverketDB', conn, if_exists='replace')
+    df.to_sql('livsmedelsverket', conn, if_exists='replace', index=True, index_label='id')
 
 print('Database created')
--- src/services/livsmedelsverket_client.py
+++ src/services/livsmedelsverket_client.py
@@ -7,8 +7,21 @@ class LivsmedelsverketClient:
         self.livsmedelsverket_conn = sqlite3.connect(ROOT_DIR / 'res/livsmedelsverket.db')
         self.livsmedelsverket_conn.row_factory = sqlite3.Row
 
+    def get_food_by_id(self, id):
+        with self.livsmedelsverket_conn as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM livsmedelsverket WHERE id = ?", (id,))
+            return cursor.fetchone()
+
     def get_food_by_number(self, number):
         with self.livsmedelsverket_conn as conn:
             cursor = conn.cursor()
             cursor.execute("SELECT * FROM livsmedelsverket WHERE Livsmedelsnummer = ?", (number,))
             return cursor.fetchone()
+
+    def get_food_by_name(self, name):
+        with self.livsmedelsverket_conn as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM livsmedelsverket WHERE Livsmedelsname = ?", (name,))
+            return cursor.fetchone()
+
